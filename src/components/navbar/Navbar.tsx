ac9645bad908f74a5acfa87f3f9f8e43255d199c
Rayane Leandro de Lima Nogueira <rayaneleandrodelima2016@gmail.com>
2026-02-26 12:50:51 -0300

    refactor: organiza estrutura dos componentes Navbar e Footer

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,1 +1,24 @@
-;
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <h2>RH Digital</h2>
+      </div>
+
+      <ul className="navbar-links">
+        <li>Home</li>
+        <li>Vagas</li>
+        <li>Sobre nós</li>
+        <li>Serviços</li>
+      </ul>
+
+      <button className="navbar-button">
+        Contate-nos
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
