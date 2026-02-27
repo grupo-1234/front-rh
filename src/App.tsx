aa9cb4e788802a46921f45e2f783a2b01fda2af9
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2026-02-27 14:37:54 -0300

    Ajustes finais, rotas, links e sobre

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
-
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import LandingPage from './pages/LandingPage'
-import { ListaAvaliacao } from './components/cards/avalicao/ListaAvaliacao'
-import ListaPeopleflow from './components/cards/peopleflow/ListaPeopleflow'
-import ListaProblema from './components/cards/problema/ListaProblema'
-import ListaSolucao from './components/cards/solucao/ListaSolucao'
-import CardPrecos from './components/cards/precos/CardPrecos'
+import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 import { Hero } from './components/home/Hero'
-import Navbar from './components/navbar/Navbar'
-import CardSobre from './components/pageSobre/CardSobre'
-
+import ListaProblema from './components/cards/problema/ListaProblema'
+import ListaSolucao from './components/cards/solucao/ListaSolucao'
+import ListaPeopleflow from './components/cards/peopleflow/ListaPeopleflow'
+import CardPrecos from './components/cards/precos/CardPrecos'
+import { ListaAvaliacao } from './components/cards/avalicao/ListaAvaliacao'
+import { ModalContato } from './components/modal/ModalContato'
+import PageSobre from './components/pageSobre/CardSobre'
 
 
 function App() {
-  return (
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [modalTitle, setModalTitle] = useState("")
+
+  const abrirModal = (titulo: string) => {
+    setModalTitle(titulo)
+    setIsModalOpen(true)
+  }
+
+  const LandingPage = () => (
     <>
-      <LandingPage/>
+      <Hero onOpenModal={() => abrirModal("Falar com Especialista")} />
+      <ListaProblema />
+      <ListaSolucao />
+      <ListaPeopleflow />
+      <CardPrecos onOpenModal={() => abrirModal("Assinar Plano")} />
+      <ListaAvaliacao />
     </>
+  )
+
+  return (
+    <BrowserRouter>
+      <Navbar onOpenModal={() => abrirModal("Assine Agora")} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/sobre-nos" element={<PageSobre />} />
+      </Routes>
+
+      <Footer />
+
+      <ModalContato 
+        isOpen={isModalOpen} 
+        onClose={() => setIsModalOpen(false)} 
+        titulo={modalTitle} 
+      />
+    </BrowserRouter>
   )
 }
 
